1510354f630d9ea9de45dd7bd84124dfe6a6013d
ganeshsuthar316-svg <ganeshsuthar316@gmail.com>
2026-07-20 14:25:41 +0530

    Create game.js

---
+++ css/js/game.js
@@ -0,0 +1,16 @@
+const game = document.getElementById("game");
+
+game.innerHTML = `
+<h1 style="
+color:white;
+text-align:center;
+margin-top:40vh;
+font-family:Arial;
+">
+🚗 Indian Car Driving Game
+<br>
+Version 0.1
+</h1>
+`;
+
+console.log("Game Started");
